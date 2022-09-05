6173b5114c3dab4432cc2f9aaceaf954f7727cf6
dreamgyf <gaoyunfeng@bilibili.com>
2022-09-05 11:44:31 +0800

    init

---
+++ FlexViewPager/build/generated/source/buildConfig/debug/com/dreamgyf/android/ui/widget/viewpager/flex/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dreamgyf.android.ui.widget.viewpager.flex;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.dreamgyf.android.ui.widget.viewpager.flex";
+  public static final String BUILD_TYPE = "debug";
+}
